4048e563d464c16f753ecf18eef5c51b2c01c80d
pramo <pramodyahk@gmail.com>
2017-12-28 22:55:35 +0530

    title bar added

--- src/app/material.imports.ts
+++ src/app/material.imports.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatCheckboxModule, MatMenuModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatMenuModule, MatToolbarModule,MatIconModule } from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatMenuModule],
-    exports: [MatButtonModule, MatCheckboxModule, MatMenuModule],
+    imports: [MatButtonModule, MatCheckboxModule, MatMenuModule, MatToolbarModule,MatIconModule],
+    exports: [MatButtonModule, MatCheckboxModule, MatMenuModule, MatToolbarModule ,MatIconModule],
 })
 export class MaterialImports { }
